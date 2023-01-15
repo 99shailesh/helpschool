a95e3ccf6aba20e0a4cf302be21b9f146c731a58
99shailesh <38884157+99shailesh@users.noreply.github.com>
2023-01-16 01:29:49 +0530

    Delete script_a.js

--- script_a.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var inpObj = document.getElementById("id1");
-  if (!inpObj.checkValidity()) {
-    document.getElementById("demo").innerHTML = inpObj.validationMessage;
-  } else {
-    document.getElementById("demo").innerHTML = "Input OK";
-  } 
-}
